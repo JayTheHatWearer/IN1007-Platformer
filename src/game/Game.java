02fcccaf50bbed454a15aa9e67a5bcca4c87f9fe
Jack Jones <jack@goodatdesign.com>
2023-03-06 02:58:25 +0000

    fixed typo

--- src/game/Game.java
+++ src/game/Game.java
@@ -22,7 +22,7 @@ public class Game {
         mainLevel = new LevelManager();
 
         view = new GameView(initLevel, 500, 500);
-        mainLevel.loadLevel(view, 2);
+        mainLevel.loadLevel(view, 0);
 
 
         GiveFocus focusChecker = new GiveFocus(view);
